14cbcbf735091a2a0c8ac68ec9281a2e8b29da8f
Jace Manshadi <jmanshad@protonmail.com>
2022-10-27 22:28:35 -0700

    forgot to remove the secret key

--- cineplex_website/cineplex/settings.py
+++ cineplex_website/cineplex/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3jkm0$homj#&45amo%h*5$h=&a_(2bh^f$v=sl*52((wpj%8!='
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
