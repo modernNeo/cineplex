89b425c5924999e7723774853bf7b26cf6c415ff
Jace Manshadi <jmanshad@protonmail.com>
2022-12-08 01:34:57 -0800

    installing the requirements for the poller in the website container so it can run the poll manually

--- cineplex_website/startup.sh
+++ cineplex_website/startup.sh
@@ -8,5 +8,8 @@ setup-timezone -z Canada/Pacific
 
 pip install --no-cache-dir -r requirements.txt
 
+# this is here cause the user has the option of manually running a poll via the website
+pip install --no-cache-dir -r cineplex_poller/requirements.txt
+
 python manage.py migrate
 python manage.py runserver 0.0.0.0:8000
