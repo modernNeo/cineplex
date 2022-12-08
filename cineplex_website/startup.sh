e48319315525f202cb6e3da26bdc8603a14dd636
Jace Manshadi <jmanshad@protonmail.com>
2022-12-08 00:47:29 -0800

    changed the port in the wrong place

--- cineplex_website/startup.sh
+++ cineplex_website/startup.sh
@@ -11,4 +11,4 @@ setup-timezone -z Canada/Pacific
 pip install --no-cache-dir -r requirements.txt
 
 python manage.py migrate
-python manage.py runserver 0.0.0.0:8001
+python manage.py runserver 0.0.0.0:8000
